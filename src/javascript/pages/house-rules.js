79319609fcbd9ff34ed9d7a9f0295df9d342b9d4
Jos <buughost@gmail.com>
2022-03-25 21:37:13 +0100

    implement profile selection

--- src/javascript/pages/house-rules.js
+++ src/javascript/pages/house-rules.js
@@ -1,6 +1,6 @@
 (function () {
-    document.querySelectorAll('.fn-collapse').forEach(function () {
-        this.addEventListener('click', function (e) {
+    document.querySelectorAll('.fn-collapse').forEach(function (el) {
+        el.addEventListener('click', function (e) {
             const elem = e.target.parentNode.parentNode.parentNode
 
             if (elem.classList.contains('collapsed')) {
@@ -8,6 +8,38 @@
             } else {
                 elem.classList.add('collapsed')
             }
-        });
-    });
-})();
+        })
+    })
+
+    document.querySelectorAll('.toggle-profile').forEach(function (el) {
+        el.addEventListener('click', function (e) {
+            const elem = e.target.dataset['profile']
+
+            const showAll = elem && elem.toString().includes('rule-all')
+            if (!elem || elem.length === 0 || showAll) {
+                document.querySelectorAll('.house-rule').forEach(function (item) {
+                    showAll
+                        ? item.classList.remove('collapsed')
+                        : item.classList.add('collapsed')
+                })
+                return
+            }
+
+            document.querySelectorAll('.house-rule').forEach(function (item) {
+                item.classList.add('collapsed')
+            })
+
+            elem.split(' ').forEach(function (item) {
+                if (item === null || item === '') {
+                    return
+                }
+                const id = document.getElementById(item)
+                if (id === null) {
+                    console.error('null ref on id: ' + item)
+                    return
+                }
+                id.classList.remove('collapsed')
+            })
+        })
+    })
+})()
