ce1c6e5b72590905f30498a6de0c01060451cf41
Zarthus <github@liefland.net>
2022-04-12 16:41:49 +0000

    Deploying to gh-pages from @ Zarthus/dnd@52076d856f6351e42475da0d4962f16bc9f94750 🚀

---
+++ resources/javascript/theme-switcher.js
@@ -0,0 +1,27 @@
+(function () {
+    const maintheme = document.getElementById('theme-default');
+    const lightmode = document.getElementById('theme-light');
+    const darkmode = document.getElementById('theme-dark');
+
+    let currentTheme = maintheme;
+    const swapTheme = function(newTheme) {
+        document.body.classList.remove(darkmode.id);
+        document.body.classList.remove(lightmode.id);
+        document.body.classList.remove(currentTheme.id);
+        currentTheme = newTheme;
+        document.body.classList.add(currentTheme.id);
+        maintheme.href = newTheme.href;
+    }
+
+    document.querySelector('#theme-switcher').addEventListener('click', function () {
+        if (this.checked) {
+            swapTheme(darkmode);
+        } else {
+            swapTheme(lightmode);
+        }
+    });
+
+    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        document.querySelector('#theme-switcher').dispatchEvent(new Event('click'));
+    }
+})();
